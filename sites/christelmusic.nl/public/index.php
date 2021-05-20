928ba55dd095ff9542429b3f0b564e03147bdc0b
Daan van Marsbergen <daan@mollie.com>
2021-05-21 00:40:17 +0200

    Album Watershed out now!

--- sites/christelmusic.nl/public/index.php
+++ sites/christelmusic.nl/public/index.php
@@ -14,14 +14,11 @@
 
 <div class="row mt-4">
     <div class="col-md-12">
-        <h1>Coming soon: Watershed</h1>
-        <p>
-            <a class="btn btn-primary" href="https://ditto.fm/watershed_christel" target="_blank" role="button">Pre-save now!</a>
-        </p>
+        <h1>Out now: Watershed</h1>
         <p>
             <img src="/assets/images/watershed.png" class="img-fluid border"/>
         </p>
-        <p>Full album <em>Watershed</em> will be available May 21st on all streaming platforms.</p>
+        <p>Full album <em>Watershed</em> is now available on all streaming platforms.</p>
         <p><em>
             1. Watershed<br />
             2. A Glimpse of Hope<br />
@@ -34,43 +31,43 @@
         </em></p>
     </div>
 </div>
-<!--            <div class="row justify-content-center">-->
-<!--                <div class="col-md-auto">-->
-<!--                    <table class="get-track">-->
-<!--                        <colgroup>-->
-<!--                            <col/>-->
-<!--                            <col style="width: 100px;"/>-->
-<!--                        </colgroup>-->
-<!--                        <tr>-->
-<!--                            <td><i class="fab fa-spotify"></i> Spotify</td>-->
-<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
-<!--                                   href="https://open.spotify.com/track/1qgANWl98NuQ5JwtJnH6eO?si=4n2AyREURFGZcgQvWKcDug">Listen</a>-->
-<!--                            </td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td><i class="fab fa-apple"></i> Apple Music</td>-->
-<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
-<!--                                   href="https://music.apple.com/nl/album/daydream-single/1563040219?l=en">Listen</a>-->
-<!--                            </td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td><i class="fab fa-deezer"></i> Deezer</td>-->
-<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
-<!--                                   href="https://deezer.page.link/48hCyySKP9ZRhcbe8">Listen</a></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td><img class="icon" src="/assets/icons/brand-tidal.svg"/> Tidal</td>-->
-<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
-<!--                                   href="https://tidal.com/browse/track/180599573">Listen</a></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td><i class="fab fa-amazon"></i> Amazon</td>-->
-<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
-<!--                                   href="https://music.amazon.com/albums/B092JR86Q7">Listen</a></td>-->
-<!--                        </tr>-->
-<!--                    </table>-->
-<!--                </div>-->
-<!--            </div>-->
+<div class="row justify-content-center">
+    <div class="col-md-auto">
+        <table class="get-track">
+            <colgroup>
+                <col/>
+                <col style="width: 100px;"/>
+            </colgroup>
+            <tr>
+                <td><i class="fab fa-spotify"></i> Spotify</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://open.spotify.com/album/5SpgF9L5ek58qnjjP9dbIm?si=KyQQ-Vt3TkWDK0EuihSpmA">Listen</a>
+                </td>
+            </tr>
+<!--            <tr>-->
+<!--                <td><i class="fab fa-apple"></i> Apple Music</td>-->
+<!--                <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                       href="https://music.apple.com/nl/album/daydream-single/1563040219?l=en">Listen</a>-->
+<!--                </td>-->
+<!--            </tr>-->
+            <tr>
+                <td><i class="fab fa-deezer"></i> Deezer</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://deezer.page.link/3R7ZbF9kZg7i1kTp6">Listen</a></td>
+            </tr>
+            <tr>
+                <td><img class="icon" src="/assets/icons/brand-tidal.svg"/> Tidal</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://tidal.com/browse/album/183080996">Listen</a></td>
+            </tr>
+<!--            <tr>-->
+<!--                <td><i class="fab fa-amazon"></i> Amazon</td>-->
+<!--                <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                       href="https://music.amazon.com/albums/B092JR86Q7">Listen</a></td>-->
+<!--            </tr>-->
+        </table>
+    </div>
+</div>
 
 <div class="row mt-5">
     <div class="col-md-12">
