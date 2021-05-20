5335f6d902a5c9b56978bdb326721273b2945b65
Daan van Marsbergen <daan@mollie.com>
2021-05-20 21:45:44 +0200

    Don't duplicate header and footer html for multiple pages

---
+++ sites/christelmusic.nl/public/cd.php
@@ -0,0 +1,9 @@
+<?php require './includes/header.html'; ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <h1>Coming soon: order your CD here</h1>
+    </div>
+</div>
+
+<?php require './includes/footer.html'; ?>
---
+++ sites/christelmusic.nl/public/index.php
@@ -0,0 +1,169 @@
+<?php require './includes/header.html'; ?>
+
+<div class="row">
+    <div class="col-md-8 offset-md-2">
+        <p class="lead">Classical pianist, composer.</p>
+        <p>Currently working on my first album 'Watershed'. Besides that, I enjoy playing (neo-)classical
+            pieces,
+            but I also like to make covers.</p>
+        <p>I have taught myself to play the piano since I was about 10 years old. Over the years I have come
+            to feel
+            very passionate about music, so I recently started piano (improvisation) lessons.</p>
+    </div>
+</div>
+
+<div class="row mt-4">
+    <div class="col-md-12">
+        <h1>Coming soon: Watershed</h1>
+        <p>
+            <a class="btn btn-primary" href="https://ditto.fm/watershed_christel" target="_blank" role="button">Pre-save now!</a>
+        </p>
+        <p>
+            <img src="/assets/images/watershed.png" class="img-fluid border"/>
+        </p>
+        <p>Full album <em>Watershed</em> will be available May 21st on all streaming platforms.</p>
+        <p><em>
+            1. Watershed<br />
+            2. A Glimpse of Hope<br />
+            3. On the Run<br />
+            4. Memories<br />
+            5. Chased by Shadows<br />
+            6. Daydream<br />
+            7. Mischievous Exploration<br />
+            8. The Vigor of the Ocean<br />
+        </em></p>
+    </div>
+</div>
+<!--            <div class="row justify-content-center">-->
+<!--                <div class="col-md-auto">-->
+<!--                    <table class="get-track">-->
+<!--                        <colgroup>-->
+<!--                            <col/>-->
+<!--                            <col style="width: 100px;"/>-->
+<!--                        </colgroup>-->
+<!--                        <tr>-->
+<!--                            <td><i class="fab fa-spotify"></i> Spotify</td>-->
+<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                                   href="https://open.spotify.com/track/1qgANWl98NuQ5JwtJnH6eO?si=4n2AyREURFGZcgQvWKcDug">Listen</a>-->
+<!--                            </td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <td><i class="fab fa-apple"></i> Apple Music</td>-->
+<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                                   href="https://music.apple.com/nl/album/daydream-single/1563040219?l=en">Listen</a>-->
+<!--                            </td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <td><i class="fab fa-deezer"></i> Deezer</td>-->
+<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                                   href="https://deezer.page.link/48hCyySKP9ZRhcbe8">Listen</a></td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <td><img class="icon" src="/assets/icons/brand-tidal.svg"/> Tidal</td>-->
+<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                                   href="https://tidal.com/browse/track/180599573">Listen</a></td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <td><i class="fab fa-amazon"></i> Amazon</td>-->
+<!--                            <td><a class="btn btn-outline-secondary" target="_blank"-->
+<!--                                   href="https://music.amazon.com/albums/B092JR86Q7">Listen</a></td>-->
+<!--                        </tr>-->
+<!--                    </table>-->
+<!--                </div>-->
+<!--            </div>-->
+
+<div class="row mt-5">
+    <div class="col-md-12">
+        <h1>Out now: Daydream</h1>
+        <p>
+            <img src="/assets/images/daydream.jpg?v2" class="img-fluid border"/>
+        </p>
+        <p>Second single <em>Daydream</em> is out now on all streaming platforms.</p>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-auto">
+        <table class="get-track">
+            <colgroup>
+                <col/>
+                <col style="width: 100px;"/>
+            </colgroup>
+            <tr>
+                <td><i class="fab fa-spotify"></i> Spotify</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://open.spotify.com/track/1qgANWl98NuQ5JwtJnH6eO?si=4n2AyREURFGZcgQvWKcDug">Listen</a>
+                </td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-apple"></i> Apple Music</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://music.apple.com/nl/album/daydream-single/1563040219?l=en">Listen</a>
+                </td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-deezer"></i> Deezer</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://deezer.page.link/48hCyySKP9ZRhcbe8">Listen</a></td>
+            </tr>
+            <tr>
+                <td><img class="icon" src="/assets/icons/brand-tidal.svg"/> Tidal</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://tidal.com/browse/track/180599573">Listen</a></td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-amazon"></i> Amazon</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://music.amazon.com/albums/B092JR86Q7">Listen</a></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<div class="row mt-5">
+    <div class="col-md-12">
+        <h1>Out now: Watershed</h1>
+        <p>
+            <img src="/assets/images/watershed.png" class="img-fluid border"/>
+        </p>
+        <p>First single <em>Watershed</em> is out now on all streaming platforms.</p>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-md-auto">
+        <table class="get-track">
+            <colgroup>
+                <col/>
+                <col style="width: 100px;"/>
+            </colgroup>
+            <tr>
+                <td><i class="fab fa-spotify"></i> Spotify</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://open.spotify.com/track/7o2vFMbCdAsS1kw30gItDn?si=99WnkJOCS6-ctVmsSE72cg">Listen</a>
+                </td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-apple"></i> Apple Music</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://music.apple.com/nl/album/watershed-single/1560090229?l=en">Listen</a>
+                </td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-deezer"></i> Deezer</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://deezer.page.link/nG5d7zfAaZDwzUP76">Listen</a></td>
+            </tr>
+            <tr>
+                <td><img class="icon" src="/assets/icons/brand-tidal.svg"/> Tidal</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://tidal.com/track/178478594">Listen</a></td>
+            </tr>
+            <tr>
+                <td><i class="fab fa-amazon"></i> Amazon</td>
+                <td><a class="btn btn-outline-secondary" target="_blank"
+                       href="https://music.amazon.com/albums/B0917G2C7V">Listen</a></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<?php require './includes/footer.html'; ?>
