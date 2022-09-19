4173304c8af1d84f613585594f8d24d539603750
Daan van Marsbergen <daan@mollie.com>
2022-09-20 01:41:34 +0200

    Automatic cache busting

--- sites/christelmusic.nl/app/public/includes/header.php
+++ sites/christelmusic.nl/app/public/includes/header.php
@@ -19,7 +19,7 @@ if (!isset($releaseProject)) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="./assets/theme.css?v7" rel="stylesheet"/>
+    <link href="./assets/theme.css?ver=<?=md5_file('./assets/theme.css')?>" rel="stylesheet"/>
 
     <script src="https://kit.fontawesome.com/f0b0cd2378.js" crossorigin="anonymous"></script>
 
